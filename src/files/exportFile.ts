1daca3472e1289f316384bacc0388b4f1d03f298
biancangming <942242856@qq.com>
2021-10-11 08:52:08 +0800

    更新repository地址

--- src/files/exportFile.ts
+++ src/files/exportFile.ts
@@ -4,11 +4,12 @@ export function saveFileFromBlob(file: any, fileName: string, contentType = "app
     }
     const link = document.createElement("a");
     const blob = new Blob([file], {
-        type: contentType,
-    }); //这个是Content-Typele的type类型（https://tool.oschina.net/commons）
+        type: contentType,//这个是Content-Typele的type类型（https://tool.oschina.net/commons）
+    });
     const url = window.URL.createObjectURL(blob);
     link.href = url;
-    link.download = fileName + ".xlsx";
+    link.download = fileName;
     link.click();
+    link.remove()
     window.URL.revokeObjectURL(url);
 }

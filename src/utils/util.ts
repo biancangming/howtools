51d23cb8f81f9760f5f33114eacca121304dba25
biancangming <942242856@qq.com>
2021-10-13 18:34:58 +0800

    增加blob转json

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -4,7 +4,7 @@
  *  @description 节流函数
  */
 export function throttle(fn: Function, delay = 500) {
-    let timer: number = null
+    let timer: NodeJS.Timeout = null
     return (...args: unknown[]) => {
         if (!timer) {
             timer = setTimeout(() => {
@@ -14,18 +14,37 @@ export function throttle(fn: Function, delay = 500) {
         }
     }
 }
+
 /**
  * @param  {Function} fn
  * @param  {} delay=500 延迟时间默认500 ms
  * @description 防抖函数
  */
- export function debounce(fn: Function, delay = 500) {
-    let timer: number = null;
+export function debounce(fn: Function, delay = 500) {
+    let timer: NodeJS.Timeout = null;
     return (...args: unknown[]) => {
         if (timer) clearTimeout(timer);
         timer = setTimeout(() => {
             fn.apply(this, args);
             timer = null;
         }, delay)
+    }
+}
+
+//字符串是否可转化为JSON
+export function isJSON(str: string) {
+    if (typeof str == 'string') {
+        try {
+            const obj = JSON.parse(str);
+            if (typeof obj == 'object' && obj) {
+                return true;
+            } else {
+                return false;
+            }
+
+        } catch (e) {
+            console.warn('error：' + str.slice(0, 100) + '!!!' + e);
+            return false;
+        }
     }
 }

01ccbc5a628da21eb7f7cd8650cfcd2e9413a4ac
biancangming <942242856@qq.com>
2023-11-14 15:29:42 +0800

    增加获取日历算发

---
+++ src/utils/calendar.ts
@@ -0,0 +1,72 @@
+/**
+ * new Date js api 
+ * 参考：https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Date
+ * 日历其实是 7 * 6 的一个常见的布局, 总计长度 42，横向是从星期日 到 星期一
+ * 1. getDate() 获取某月第几天，从 1 开始 1-31 
+ * 2. getFullYear() 获取某一年， 四位数年份
+ * 3. getDay() 获取一周第几天， 0 - 6
+ * 4. getHours() 根据本地时间 ，返回一个指定的 Date 对象的小时
+ * 5. getMinutes() 获取分钟 0 - 59
+ * 6. getSeconds() 获取秒 0 - 59
+*/
+
+export interface GetCalendarByDateOpts {
+    startByMonday?: boolean, // 是否从周一开始统计 
+    ignoreLastWeek?: boolean, // 是否省略最后一周全部为下一个月的情况
+}
+
+/**
+ * @param date 时间，如 2023-11-14  、 2023-11 等
+ * @param opts GetCalendarByDateOpts
+ * @description 根据年月生成日历，农历可通过 https://github.com/jjonline/calendar.js 进行转换
+*/
+export function getCalendarByDate(date: string, opts?: GetCalendarByDateOpts) {
+    const startByMonday = opts?.startByMonday || false
+    const ignoreLastWeek = opts?.ignoreLastWeek || false
+
+    const calendar: {
+        date: Date,
+        isCurrentMonth: boolean,
+        year: number,
+        month: number,
+        day: number,
+        weekday: number,
+    }[] = []
+
+    const _date = new Date(date)
+    const year = _date.getFullYear()
+    const month = _date.getMonth() // 第几个月
+    const day = _date.getDay() // 星期几
+
+    const dayjs = new Date(`${year}-${month + 1}-01`)
+
+    // 如果日期不是周日，重置dayjs到正常日历第一天。
+    if (day > 0) {
+        dayjs.setDate(dayjs.getDate() - (day + (startByMonday ? 0 : 1)))
+    }
+
+    // 补全正常数据
+    let count = 0
+    while (calendar.length < 7 * 6) {
+        const _dayjs = new Date(dayjs)
+        _dayjs.setDate(dayjs.getDate() + count)
+
+        if(ignoreLastWeek && count > 7 * 5){
+            break
+        }
+
+        calendar.push({
+            date: _dayjs,
+            year: _dayjs.getFullYear(),
+            month: _dayjs.getMonth() + 1,
+            day: _dayjs.getDate(),
+            weekday: _dayjs.getDay(),
+            isCurrentMonth: _dayjs.getMonth() === month,
+        })
+
+        count++
+    }
+
+    return calendar
+}
+
--- src/utils/util.test.ts
+++ src/utils/util.test.ts
@@ -2,6 +2,7 @@ import { typeOf, isRegExp, isNumber, isEmpty } from './type';
 import { debounce } from './util';
 import { md5 } from './md5';
 import { Base64 } from './base64'
+import { getCalendarByDate } from './calendar'
 
 //测试防抖函数
 
@@ -37,13 +38,13 @@ console.log("/.*?/ is", typeOf(/.*?/))
 console.log(isRegExp(/.*?/))
 console.log(isNumber(0))
 console.log("===================空对象验证测试=============================")
-console.log("[] is == >",isEmpty([]))
-console.log("{} is == >",isEmpty({}))
-console.log("0 is == >",isEmpty(0))
-console.log("\" \" is == >",isEmpty(" "))
-console.log("\"\" is == >",isEmpty(""))
-console.log("null is == >",isEmpty(null))
-console.log("undefined is == >",isEmpty(undefined))
+console.log("[] is == >", isEmpty([]))
+console.log("{} is == >", isEmpty({}))
+console.log("0 is == >", isEmpty(0))
+console.log("\" \" is == >", isEmpty(" "))
+console.log("\"\" is == >", isEmpty(""))
+console.log("null is == >", isEmpty(null))
+console.log("undefined is == >", isEmpty(undefined))
 
 //MD5 校验
 console.log("===================MD5 校验测试=============================")
@@ -54,3 +55,10 @@ console.log("===================Base64 校验测试=============================
 let base64 = new Base64()
 const imgUrl = 'M00/00/7A/rBABFWIEx1OAAMRSAAAgbEov6ck276.jpg'
 console.log(base64.encode(imgUrl))
+
+
+// 日历
+console.log("===================日历 校验测试=============================")
+getCalendarByDate("2023-11-14")
+const x = getCalendarByDate("2023-11-14", { startByMonday: true })
+console.log(getCalendarByDate("2023-11", { ignoreLastWeek: true }))
